8372f418f9e2721ac4ab9488dfa27f7617c031e6
2410514010-pixel <2410514010@mahasiswa.upnvj.ac.id>
2025-12-04 23:26:50 +0700

    Update Peminjaman.php

--- app/Controllers/Peminjaman.php
+++ app/Controllers/Peminjaman.php
@@ -5,3 +5,34 @@ public function riwayat()
 
     return view('riwayat', $data);
 }
+
+public function surat($id_peminjaman)
+{
+    $model = new \App\Models\PeminjamanModel();
+    $data = $model->find($id_peminjaman);
+
+    if (!$data) {
+        throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound("Data peminjaman tidak ditemukan");
+    }
+
+    return view('surat_peminjaman', $data);
+}
+
+public function download($id_peminjaman)
+{
+    $dompdf = new \Dompdf\Dompdf();
+    $model = new \App\Models\PeminjamanModel();
+
+    $data = $model->find($id_peminjaman);
+    if (!$data) {
+        throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound("Data tidak ditemukan");
+    }
+
+    $html = view('surat_peminjaman', $data);
+
+    $dompdf->loadHtml($html);
+    $dompdf->setPaper('A4', 'portrait');
+    $dompdf->render();
+
+    return $dompdf->stream("Surat-Peminjaman.pdf", ["Attachment" => true]);
+}
