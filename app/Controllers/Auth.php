0caee4043ef0ce3197184050abc3448e0c775dca
alifa3191-rgb <alifa3191@gmail.com>
2025-12-06 13:48:13 +0700

    Add PetugasModel for login process validation

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 use App\Models\UserModel;
+use App\Models\PetugasModel; 
 
 class Auth extends BaseController
 {
@@ -14,25 +15,49 @@ class Auth extends BaseController
     {
         $session = session();
         $userModel = new UserModel();
+        $petugasModel = new PetugasModel(); 
 
         $nim = $this->request->getVar('nim');
         $password = $this->request->getVar('password');
         $remember = $this->request->getVar('remember');
 
+        // ===== CEK PETUGAS DARI TABEL petugas TERLEBIH DAHULU =====
+        $petugas = $petugasModel->getByNIM($nim);
+
+        if ($petugas && $password == $petugas['password']) {
+
+            // SET SESSION 
+            $session->set([
+                'nim' => $petugas['NIM'],
+                'logged_in' => true,
+                'last_activity' => time()
+            ]);
+
+            // REMEMBER ME 
+            if ($remember) {
+                setcookie("nim", $nim, time() + 86400, "/");
+                setcookie("password", $password, time() + 86400, "/");
+            } else {
+                setcookie("nim", "", time() - 3600, "/");
+                setcookie("password", "", time() - 3600, "/");
+            }
+
+            return redirect()->to('/dashboard_petugas')->with('success', 'Login Petugas Berhasil!');
+        }
+
+        // ===== CEK USER BIASA =====
         $user = $userModel->getByNIM($nim);
 
         if ($user && $password == $user['password']) {
 
             // SET SESSION
             $session->set([
-                'id_user' => $user['id_user'],
-                'nama' => $user['nama'],
                 'nim' => $user['NIM'],
                 'logged_in' => true,
                 'last_activity' => time()
             ]);
 
-            // REMEMBER ME
+            // REMEMBER ME 
             if ($remember) {
                 setcookie("nim", $nim, time() + 86400, "/");
                 setcookie("password", $password, time() + 86400, "/");
